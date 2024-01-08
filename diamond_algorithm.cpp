9e4f9924122ffd9f368083e4eb766aef7c2283ed
ant1234 <antonyvidetta1986@gmail.com>
2024-01-08 18:38:43 +1300

    diamond algorithm complete - working

--- diamond_algorithm.cpp
+++ diamond_algorithm.cpp
@@ -7,16 +7,18 @@ int main()
 
     // Problem 5: Write a program to print the diamond star(*) pattern based on an input number. 
     int input = 0;
-    std::cout << "Please enter size for the diamond by entering a number followed by a space and another number : ";
+    std::cout << "Please an odd number : ";
     std::cin >> input;
 
-    int half = input / 2;
-
+    int halfDiamond = input / 2;
+    int bottomTriangle = (2 * halfDiamond + 1);
+    int bottomSpaces = (input - halfDiamond - 1);
 
     for (int i = 0; i < input; i++) { 
 
-    
-        if(i < half) {
+        // top half of the diamond.
+        if(i < halfDiamond) {
+
             for (int j = 0; j < (input - i - 1); j++) { // spaces 
                 std::cout << " ";  
             }
@@ -24,24 +26,23 @@ int main()
             for (int j = 0; j < (2 * i + 1); j++) { // stars 
                 std::cout << "*";  
             }
+
+        // bottom half of the diamond.
         } else {
 
-            // for (int j = 0; j < (input + i + 1); j++) { // spaces 
-            //     std::cout << " ";  
-            // }
+            for (int j = 0; j < bottomSpaces; j++) { // spaces 
+                std::cout << " ";  
+            }
 
-            
-            for (int j = 0; j < (2 * i - 2); j++) { // stars 
+            for (int j = 0; j < bottomTriangle; j++) { // stars 
                 std::cout << "*";  
             }
 
+            bottomTriangle = bottomTriangle - 2;
+            bottomSpaces = bottomSpaces + 1;
 
         }
-
-
         std::cout << std::endl;
-
     }
-
     return 0;
 }
