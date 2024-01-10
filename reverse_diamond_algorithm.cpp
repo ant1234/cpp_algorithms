5e66f5d360a31311c177ca8f7f2e63483f251ec8
ant1234 <antonyvidetta1986@gmail.com>
2024-01-10 18:29:52 +1300

    reverse diamond algorithm working - complete

--- reverse_diamond_algorithm.cpp
+++ reverse_diamond_algorithm.cpp
@@ -4,7 +4,7 @@
 
 int main() {
 
-    // Problem 5: Write a program to print the reverse of the diamond star(*) pattern based on an input number. 
+    // Problem 6: Write a program to print the reverse of the diamond star(*) pattern based on an input number. 
 
     /* 
         ********** 10 stars and 0 spaces
@@ -20,10 +20,12 @@ int main() {
     
     */
     int input = 0;
-    std::cout << "Please an even number : ";
+    std::cout << "Please entern an even number : ";
     std::cin >> input;
 
     int halfDiamond = input / 2;
+    int bottomStars = halfDiamond;
+    int bottomSpaces = halfDiamond;
 
     for(int i = 0; i < input; i++) {
 
@@ -46,8 +48,27 @@ int main() {
                 std::cout << "*";
             }
 
-        } 
-        
+        // bottom half of the diamond.
+        } else {
+
+            for(int stars_row = 0; stars_row < (i - halfDiamond) + 1; stars_row++) {
+                std::cout << "*";
+            }
+
+            for(int spaces_row = 0; spaces_row < (input - i) - 1; spaces_row++) {
+                std::cout << " ";
+            }
+
+            for(int spaces_row = 0; spaces_row < (input - i) - 1; spaces_row++) {
+                std::cout << " ";
+            }
+
+            for(int stars_row = 0; stars_row < (i - halfDiamond) + 1; stars_row++) {
+                std::cout << "*";
+            }
+
+        }
+
         std::cout << std::endl;
 
     }
