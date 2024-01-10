192f4c48cdb0a6353c51cef77ab7e29d0bad2278
ant1234 <antonyvidetta1986@gmail.com>
2024-01-10 16:58:14 +1300

    reverse diamond algorithm - first half working

---
+++ reverse_diamond_algorithm.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+
+    // Problem 5: Write a program to print the reverse of the diamond star(*) pattern based on an input number. 
+
+    /* 
+        ********** 10 stars and 0 spaces
+        ****  **** 8 stars and 2 spaces
+        ***    *** 6 stars and 4 spaces
+        **      ** 4 stars and 6 spaces
+        *        * 2 stars and 8 spaces
+        *        * 2 stars and 8 spaces 
+        **      ** 4 stars and 6 spaces
+        ***    *** 6 stars and 4 spaces
+        ****  **** 8 stars and 2 spaces
+        ********** 10 stars and 0 sapces
+    
+    */
+    int input = 0;
+    std::cout << "Please an even number : ";
+    std::cin >> input;
+
+    int halfDiamond = input / 2;
+
+    for(int i = 0; i < input; i++) {
+
+        // top half of the diamond.
+        if(i < halfDiamond) {
+
+            for(int stars_row = 0; stars_row < halfDiamond - i; stars_row++) {
+                std::cout << "*";
+            }
+
+            for(int spaces_row = 0; spaces_row < i; spaces_row++) {
+                std::cout << " ";
+            }
+            
+            for(int spaces_row = 0; spaces_row < i; spaces_row++) {
+                std::cout << " ";
+            }
+
+            for(int stars_row = 0; stars_row < halfDiamond - i; stars_row++) {
+                std::cout << "*";
+            }
+
+        } 
+        
+        std::cout << std::endl;
+
+    }
+
+    return 0;
+}
