502cdde7ea38a5938cb3936f0cb788eeddb93cc3
ant1234 <antonyvidetta1986@gmail.com>
2024-01-21 12:21:52 +1300

    game of eight - working

--- game_of_eight.cpp
+++ game_of_eight.cpp
@@ -19,7 +19,7 @@ Write a program to solve this problem.
 1. main game loop
 2. begin game 
 3. player1 chosen input, player 2 chosen input 
-4. create a const for the number 8 called INITIAL_NUMBER
+4. create a variable for the number 8 called initialNum
 5. offer player a prompt with numbers available in range 
 5. check players choice, return error if out of range 
 6. initial number is subtracted by chosen number
@@ -39,6 +39,14 @@ functions :
 #include <iostream>
 #include <string>
 
+int initialNum = 8;
+int player1Input;
+int player2Input;
+bool playerSwitch = false;
+int numbersAvailable[4] = {1, 2, 3, 4};
+int arraySize = sizeof(numbersAvailable) / sizeof(numbersAvailable[0]);
+int removeIndex = -1;
+
 void playGame();
 bool playAgain();
 
@@ -54,7 +62,86 @@ int main() {
 }
 
 void playGame() {
-    std::cout << "Play the game" << std::endl;
+    
+    if(!playerSwitch) {
+
+        std::cout << "Player 1, please choose one of these numbers and press enter : " << std::endl;
+
+        for (int i = 0; i < arraySize; i++) {
+            std::cout << numbersAvailable[i] << std::endl;
+        }
+ 
+        std::cin >> player1Input;
+
+        initialNum = initialNum - player1Input;
+
+        if(initialNum == 0) {
+            std::cout << "Player 1 is the winner!" << std::endl;
+        }
+
+        for (int i = 0; i < arraySize; ++i) {
+
+            if (numbersAvailable[i] == player1Input) {
+                removeIndex = i;
+                break;
+            }
+        }
+
+        if (removeIndex != -1) {
+
+            for (int i = removeIndex; i < arraySize - 1; ++i) {
+                numbersAvailable[i] = numbersAvailable[i + 1];
+            }
+
+            arraySize--;
+            std::cout << std::endl;
+
+        } else {
+            std::cout << "Number not found in the array." << std::endl;
+        }
+        
+        playerSwitch = true;
+
+    } else {
+
+        std::cout << "Player 2, please choose one of these numbers and press enter : " << std::endl;
+        
+        for (int i = 0; i < arraySize; i++) {
+            std::cout << numbersAvailable[i] << std::endl;
+        }
+
+        std::cin >> player2Input;
+
+        initialNum = initialNum - player2Input;
+
+
+        if(initialNum < 0) {
+            std::cout << "Player 2 is the winner!" << std::endl;
+        }
+
+        for (int i = 0; i < arraySize; ++i) {
+
+            if (numbersAvailable[i] == player2Input) {
+                removeIndex = i;
+                break;
+            }
+        }
+
+        if (removeIndex != -1) {
+
+            for (int i = removeIndex; i < arraySize - 1; ++i) {
+                numbersAvailable[i] = numbersAvailable[i + 1];
+            }
+
+            arraySize--;
+            std::cout << std::endl;
+
+        } else {
+            std::cout << "Number not found in the array." << std::endl;
+        }
+
+        playerSwitch = false; 
+    }
 }
 
 bool playAgain() {
